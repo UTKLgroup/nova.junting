6899f3936888f34758d62a8de01a29a5342e6ed9
juntingh <junting.huang@gmail.com>
2017-10-30 15:26:37 -0500

    added scan(), single(), and input arguments

--- slicer/evd.py
+++ slicer/evd.py
@@ -1,18 +1,34 @@
 from subprocess import call
 from datetime import datetime
 from time import sleep
+import argparse
+
 
 x0 = 6
 y0 = 131
 width = 1247
 height = 747
-figure_dir = 'data/evd/fd_data_cosmic'
-prefix = 'fd_data_cosmic'
 
-for i in range(100):
+
+def scan(figure_dir, prefix):
+    for i in range(100):
+        time = datetime.now().strftime('%Y_%m_%d.%H_%M_%S')
+        print(time)
+        call('screencapture -x -R{},{},{},{} {}/{}{}.png'.format(x0, y0, width, height, figure_dir, prefix, time), shell=True)
+        sleep(10)
+    print('Finished.')
+
+
+def single(figure_dir, prefix):
     time = datetime.now().strftime('%Y_%m_%d.%H_%M_%S')
-    print(time)
-    call('screencapture -x -R{},{},{},{} {}/{}.{}.png'.format(x0, y0, width, height, figure_dir, prefix, time), shell=True)
-    sleep(10)
+    call('screencapture -x -R{},{},{},{} {}/{}{}.png'.format(x0, y0, width, height, figure_dir, prefix, time), shell=True)
 
-print('Finished.')
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--figure_dir', help='figure dir', default='.')
+    parser.add_argument('-p', '--prefix', help='figure name prefix', default='')
+    args = parser.parse_args()
+
+    prefix = args.prefix if args.prefix.endswith('.') else args.prefix + '.'
+    single(args.figure_dir, args.prefix)
