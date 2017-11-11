641e4aecd3d26adf89a276c1b44cc8d555d7700a
juntingh <junting.huang@gmail.com>
2017-11-10 22:02:14 -0600

    added join()

--- slicer/evd.py
+++ slicer/evd.py
@@ -3,6 +3,8 @@ from datetime import datetime
 from time import sleep
 import argparse
 import pyautogui
+import cv2
+import numpy as np
 
 
 x0 = 6
@@ -35,10 +37,32 @@ def click():
 
 def single_click(figure_dir, prefix):
     pyautogui.hotkey('command', 'tab')
+    # click()
     for i in range(200):
         single(figure_dir, prefix=prefix, suffix=str(i))
         click()
-        sleep(10)
+        sleep(12)
+
+
+def join(figure_dir, prefix):
+    for i in range(200):
+        print('processing event ', i)
+        raw = cv2.imread('{}/{}.{}.png'.format(figure_dir, prefix, i))
+        slicer4d = cv2.imread('{}/{}.slicer4d.{}.png'.format(figure_dir, prefix, i))
+        tdslicer_2d = cv2.imread('{}/{}.tdslicer.2d.{}.png'.format(figure_dir, prefix, i))
+        tdslicer_merge = cv2.imread('{}/{}.tdslicer.merge.{}.png'.format(figure_dir, prefix, i))
+
+        top = np.concatenate((raw, slicer4d), axis=1)
+        bottom = np.concatenate((tdslicer_2d, tdslicer_merge), axis=1)
+        full = np.concatenate((top, bottom), axis=0)
+
+        cv2.putText(full, 'raw', (5, 620), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
+        cv2.putText(full, 'Slicer4D', (1250, 620), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
+        cv2.putText(full, 'TDSlicer (2D)', (5, 1365), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
+        cv2.putText(full, 'TDSlicer (merge)', (1250, 1365), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
+
+        cv2.imwrite('{}/{}.all.{}.png'.format(figure_dir, prefix, i), full)
+        # cv2.imwrite('test.png', full)
 
 
 if __name__ == '__main__':
@@ -52,3 +76,4 @@ if __name__ == '__main__':
     # pyautogui.hotkey('command', 'tab')
     # single(args.figure_dir, prefix=args.prefix)
     single_click(args.figure_dir, prefix=args.prefix)
+    # join(args.figure_dir, prefix=args.prefix)
