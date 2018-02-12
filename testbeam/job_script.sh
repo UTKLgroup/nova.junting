43fe50c2302ce52eff82035d46756d2d1bf3977b
juntingh <junting.huang@gmail.com>
2018-02-12 11:47:44 -0600

    updated job_script.sh and job_submit.sh to adapt to the unmounting of bluearc

--- testbeam/job_script.sh
+++ testbeam/job_script.sh
@@ -1,14 +1,29 @@
+
+function setup_nova_grid {
+  source /cvmfs/nova.opensciencegrid.org/novasoft/slf6/novasoft/setup/setup_nova.sh "$@"
+}
+
+setup_nova_grid -r S18-01-19 -6 /cvmfs/nova.opensciencegrid.org/novasoft/slf6/novasoft -e /cvmfs/nova.opensciencegrid.org/externals -b maxopt
 source /nova/app/users/junting/g4beamline/G4beamline-3.04/bin/g4bl-setup.sh
-source /grid/fermiapp/products/common/etc/setups.sh
-setup jobsub_client
-setup ifdhc
 
-jobsize=10000
-first=$((${PROCESS} * ${jobsize}))
-last=$((${first} + $jobsize - 1))
+export G4BL_DIR=/nova/app/users/junting/g4beamline/G4beamline-3.04
+export G4ABLADATA=/nova/app/users/junting/g4beamline/Geant4Data/G4ABLA3.0
+export G4LEDATA=/nova/app/users/junting/g4beamline/Geant4Data/G4EMLOW6.50
+export G4ENSDFSTATEDATA=/nova/app/users/junting/g4beamline/Geant4Data/G4ENSDFSTATE2.1
+export G4NEUTRONHPDATA=/nova/app/users/junting/g4beamline/Geant4Data/G4NDL4.5
+export G4NEUTRONXSDATA=/nova/app/users/junting/g4beamline/Geant4Data/G4NEUTRONXS1.4
+export G4PIIDATA=/nova/app/users/junting/g4beamline/Geant4Data/G4PII1.3
+export G4SAIDXSDATA=/nova/app/users/junting/g4beamline/Geant4Data/G4SAIDDATA1.1
+export G4LEVELGAMMADATA=/nova/app/users/junting/g4beamline/Geant4Data/PhotonEvaporation4.3
+export G4RADIOACTIVEDATA=/nova/app/users/junting/g4beamline/Geant4Data/RadioactiveDecay5.1
+export G4REALSURFACEDATA=/nova/app/users/junting/g4beamline/Geant4Data/RealSurface1.0
 
-ifdh cp /nova/app/users/junting/testbeam/beam/beam.in input
-g4bl input first=$first last=$last
+ifdh cp /pnfs/nova/persistent/users/junting/testbeam/merge_tree.py ./merge_tree.py
+ifdh cp /pnfs/nova/persistent/users/junting/testbeam/beam.in ./beam.in
+g4bl beam.in
+python merge_tree.py beam.root
 
-chmod 777 beam.root
-ifdh cp beam.root /pnfs/nova/scratch/users/junting/g4bl/beam_${PROCESS}.root
+chmod 766 beam.root
+chmod 766 MergedAtStartLinebeam.root
+ifdh cp beam.root /pnfs/nova/scratch/users/junting/g4bl/beam.root
+ifdh cp MergedAtStartLinebeam.root /pnfs/nova/scratch/users/junting/g4bl/MergedAtStartLinebeam.root
--- testbeam/job_submit.sh
+++ testbeam/job_submit.sh
@@ -1,2 +1,1 @@
-# jobsub_submit.py -G nova --memory=4GB --expected-lifetime=25h -N 2 --OS=SL5,SL6 --resource-provides=usage_model=DEDICATED,OPPORTUNISTIC file://$PWD/Script.sh
-jobsub_submit.py -G nova -N 360 --OS=SL5,SL6 --resource-provides=usage_model=DEDICATED,OPPORTUNISTIC file://$PWD/Script.sh
+jobsub_submit -G nova -N 1 --resource-provides=usage_model=DEDICATED,OPPORTUNISTIC --role=Analysis --expected-lifetime=1h --memory=1000MB file://$PWD/job_script.sh
