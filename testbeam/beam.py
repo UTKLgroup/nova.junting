548b2e02d7f0fcff2f959847042f4f4c960b2bfb
juntingh <junting.huang@gmail.com>
2018-02-13 19:16:19 -0600

    added rotate_updownstream, position the upstream/downstream components based on rotation

--- testbeam/beam.py
+++ testbeam/beam.py
@@ -5,6 +5,7 @@ magnet_theta_relative = 10.      # degree
 
 inch = 25.4
 kill = 1
+radian_per_degree = pi / 180.
 
 target_positions = [0., 0., 0.]
 target_slab_dimensions = [31.75, 209.55, 6.35] # [height, length, width]
@@ -95,6 +96,16 @@ def rotate_y(positions, x0, z0, theta):
     positions[2] = z_rotate
 
 
+def rotate_y_absolute(positions, theta, distance, axis):
+    # theta in degree
+    positions[0] = sin(theta * radian_per_degree) * distance + axis[0]
+    positions[2] = cos(theta * radian_per_degree) * distance + axis[2]
+
+
+def get_distance(v, v0):
+    return ((v[0] - v0[0])**2 + (v[1] - v0[1])**2 + (v[2] - v0[2])**2)**0.5
+
+
 def move_collimator_upstream():
     # move z, rotate
     for collimator_upstream_part in collimator_upstream_parts:
@@ -108,38 +119,60 @@ def move_collimator_upstream():
         translate(collimator_upstream_part, [offset, 0., 0.])
 
 
+def rotate_updownstream():
+    tof_upstream_distance = get_distance(tof_upstream_positions, target_positions)
+    wire_chamber_1_distance = get_distance(wire_chamber_1_positions, target_positions)
+    wire_chamber_2_distance = get_distance(wire_chamber_2_positions, target_positions)
+    magnet_distance = get_distance(magnet_positions, target_positions)
+
+    wire_chamber_3_distance = get_distance(wire_chamber_3_positions, magnet_positions)
+    collimator_downstream_distance = get_distance(collimator_downstream_positions, magnet_positions)
+    wire_chamber_4_distance = get_distance(wire_chamber_4_positions, magnet_positions)
+    tof_downstream_distance = get_distance(tof_downstream_positions, magnet_positions)
+
+    rotate_y_absolute(tof_upstream_positions, collimator_upstream_theta, tof_upstream_distance, target_positions)
+    rotate_y_absolute(wire_chamber_1_positions, collimator_upstream_theta, wire_chamber_1_distance, target_positions)
+    rotate_y_absolute(wire_chamber_2_positions, collimator_upstream_theta, wire_chamber_2_distance, target_positions)
+    rotate_y_absolute(magnet_positions, collimator_upstream_theta, magnet_distance, target_positions)
+
+    rotate_y_absolute(wire_chamber_3_positions, wire_chamber_3_theta, wire_chamber_3_distance, magnet_positions)
+    rotate_y_absolute(collimator_downstream_positions, collimator_downstream_theta, collimator_downstream_distance, magnet_positions)
+    rotate_y_absolute(wire_chamber_4_positions, wire_chamber_3_theta, wire_chamber_4_distance, magnet_positions)
+    rotate_y_absolute(tof_downstream_positions, tof_downstream_theta, tof_downstream_distance, magnet_positions)
+
+
 def write():
     with open('beam.py.in', 'w') as f_beam:
         f_beam.write('physics QGSP_BIC\n')
         f_beam.write('param worldMaterial=Air\n')
         f_beam.write('param histoFile=beam.root\n')
         f_beam.write('param inch=25.4\n')
-    
+
         f_beam.write('g4ui when=4 "/vis/viewer/set/viewpointVector 0 1 0"\n')
         f_beam.write('g4ui when=4 "/vis/viewer/set/style wireframe"\n')
         # f_beam.write('g4ui when=4 "/vis/viewer/set/background 1 1 1"\n')
-    
+
         f_beam.write('beam gaussian particle=pi+ firstEvent=0 lastEvent=1 sigmaX=2.0 sigmaY=2.0 beamZ=-500.0 meanMomentum=64000.0\n')
         f_beam.write('trackcuts keep=pi+,pi-,pi0,kaon+,kaon-,mu+,mu-,e+,e-,gamma,proton,anti_proton\n')
-    
+
         f_beam.write('box slab height={} length={} width={} material=Cu color=1,0.01,0.01\n'.format(target_slab_dimensions[0], target_slab_dimensions[1], target_slab_dimensions[2]))
         for i in range(-2, 3):
             f_beam.write('place slab rename=target_slab_{} x={} y={} z={}\n'.format(i, target_positions[0] + i * target_delta_x, target_positions[1], -i * target_delta_z + target_positions[2]))
-    
+
         f_beam.write('box collimator_upstream_base height={} length={} width={} material=Fe color=0,1,1 kill={}\n'.format(collimator_upstream_base_dimensions[0], collimator_upstream_base_dimensions[1], collimator_upstream_base_dimensions[2], kill))
         f_beam.write('box collimator_upstream_bottom height={} length={} width={} material=Fe color=0,1,1 kill={}\n'.format(collimator_upstream_bottom_dimensions[0], collimator_upstream_bottom_dimensions[1], collimator_upstream_bottom_dimensions[2], kill))
         f_beam.write('box collimator_upstream_middle height={} length={} width={} material=Fe color=0,1,1 kill={}\n'.format(collimator_upstream_middle_dimensions[0], collimator_upstream_middle_dimensions[1], collimator_upstream_middle_dimensions[2], kill))
         f_beam.write('box collimator_upstream_top height={} length={} width={} material=Fe color=0,1,1 kill={}\n'.format(collimator_upstream_top_dimensions[0], collimator_upstream_top_dimensions[1], collimator_upstream_top_dimensions[2], kill))
-    
+
         f_beam.write('place collimator_upstream_base rename=collimator_upstream_base x={} y={} z={} rotation=y{}\n'.format(collimator_upstream_base_positions[0], collimator_upstream_base_positions[1], collimator_upstream_base_positions[2], collimator_upstream_base_theta))
         f_beam.write('place collimator_upstream_bottom rename=collimator_upstream_bottom x={} y={} z={} rotation=y{}\n'.format(collimator_upstream_bottom_positions[0], collimator_upstream_bottom_positions[1], collimator_upstream_bottom_positions[2], collimator_upstream_base_theta))
         f_beam.write('place collimator_upstream_middle rename=collimator_upstream_middle_1 x={} y={} z={} rotation=y{}\n'.format(collimator_upstream_middle_1_positions[0], collimator_upstream_middle_1_positions[1], collimator_upstream_middle_1_positions[2], collimator_upstream_middle_1_theta))
         f_beam.write('place collimator_upstream_middle rename=collimator_upstream_middle_2 x={} y={} z={} rotation=y{}\n'.format(collimator_upstream_middle_2_positions[0], collimator_upstream_middle_2_positions[1], collimator_upstream_middle_2_positions[2], collimator_upstream_middle_2_theta))
         f_beam.write('place collimator_upstream_top rename=collimator_upstream_top x={} y={} z={} rotation=y{}\n'.format(collimator_upstream_top_positions[0], collimator_upstream_top_positions[1], collimator_upstream_top_positions[2], collimator_upstream_base_theta))
-    
+
         f_beam.write('virtualdetector tof_upstream  height={} length={} width={} material=LUCITE color=0.05,0.05,0.93\n'.format(tof_upstream_dimensions[0], tof_upstream_dimensions[1], tof_upstream_dimensions[2]))
         f_beam.write('place tof_upstream rename=tof_upstream x={} y={} z={} rotation=z45,y{}\n'.format(tof_upstream_positions[0], tof_upstream_positions[1], tof_upstream_positions[2], tof_upstream_theta))
-    
+
         f_beam.write('group wire_chamber\n')
         f_beam.write('  virtualdetector wire_chamber_detector height={} length={} width={} color=0,1,0\n'.format(wire_chamber_detector_dimensions[0], wire_chamber_detector_dimensions[1], wire_chamber_detector_dimensions[2]))
         f_beam.write('  box wire_chamber_frame_vertical height={} length={} width={} color=1,0,1 kill={} material=Al\n'.format(wire_chamber_frame_vertical_dimensions[0], wire_chamber_frame_vertical_dimensions[1], wire_chamber_frame_vertical_dimensions[2], kill))
@@ -152,12 +185,12 @@ def write():
         f_beam.write('endgroup\n')
         f_beam.write('place wire_chamber rename=wire_chamber_1 x={} y={} z={} rotation=y{}\n'.format(wire_chamber_1_positions[0], wire_chamber_1_positions[1], wire_chamber_1_positions[2], wire_chamber_1_theta))
         f_beam.write('place wire_chamber rename=wire_chamber_2 x={} y={} z={} rotation=y{}\n'.format(wire_chamber_2_positions[0], wire_chamber_2_positions[1], wire_chamber_2_positions[2], wire_chamber_2_theta))
-    
+
         f_beam.write('genericbend M1 fieldHeight={} fieldLength={} fieldWidth={} kill={} ironColor=1,0,0 ironHeight={} ironLength={} ironWidth={}\n'.format(magnet_field_dimensions[0], magnet_field_dimensions[1], magnet_field_dimensions[2], kill, magnet_iron_dimensions[0], magnet_iron_dimensions[1], magnet_iron_dimensions[2]))
         f_beam.write('place M1 By={} x={} y={} z={} rotation=Y{}\n'.format(magnet_by, magnet_positions[0], magnet_positions[1], magnet_positions[2], magnet_theta))
-    
+
         f_beam.write('place wire_chamber rename=wire_chamber_3 x={} y={} z={} rotation=y{}\n'.format(wire_chamber_3_positions[0], wire_chamber_3_positions[1], wire_chamber_3_positions[2], wire_chamber_3_theta))
-    
+
         f_beam.write('group collimator_downstream\n')
         f_beam.write('  box collimator_downstream_bottom height={} length={} width={} material=Fe color=0,1,1 kill={}\n'.format(collimator_downstream_bottom_dimensions[0], collimator_downstream_bottom_dimensions[1], collimator_downstream_bottom_dimensions[2], kill))
         f_beam.write('  box collimator_downstream_middle height={} length={} width={} material=Fe color=0,.8,1 kill={}\n'.format(collimator_downstream_middle_dimensions[0], collimator_downstream_middle_dimensions[1], collimator_downstream_middle_dimensions[2], kill))
@@ -167,12 +200,13 @@ def write():
         f_beam.write('  place collimator_downstream_bottom rename=+_top x={} y={} z={}\n'.format(collimator_downstream_top_positions[0], collimator_downstream_top_positions[1], collimator_downstream_top_positions[2]))
         f_beam.write('endgroup\n')
         f_beam.write('place collimator_downstream x={} y={} z={} rotation=y{}\n'.format(collimator_downstream_positions[0], collimator_downstream_positions[1], collimator_downstream_positions[2], collimator_downstream_theta))
-    
+
         f_beam.write('place wire_chamber rename=wire_chamber_4 x={} y={} z={} rotation=y{}\n'.format(wire_chamber_4_positions[0], wire_chamber_4_positions[1], wire_chamber_4_positions[2], wire_chamber_4_theta))
-    
+
         f_beam.write('virtualdetector tof_downstream height={} length={} width={} material=LUCITE color=0.05,0.05,0.93\n'.format(tof_downstream_dimensions[0], tof_downstream_dimensions[1], tof_downstream_dimensions[2]))
         f_beam.write('place tof_downstream rename=tof_downstream x={} y={} z={} rotation=z90,y{}\n'.format(tof_downstream_positions[0], tof_downstream_positions[1], tof_downstream_positions[2], tof_downstream_theta))
 
 
 move_collimator_upstream()
+rotate_updownstream()
 write()
