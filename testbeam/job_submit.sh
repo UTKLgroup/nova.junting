0229f6d722f58e5b4c43b12dfabde60b4c27ab5b
juntingh <junting.huang@gmail.com>
2018-03-19 15:24:47 -0500

    updated testbeam/job_script.sh to submit more spills

--- testbeam/job_submit.sh
+++ testbeam/job_submit.sh
@@ -1,1 +1,1 @@
-jobsub_submit -G nova -N 300 --resource-provides=usage_model=DEDICATED,OPPORTUNISTIC --role=Analysis --expected-lifetime=3h --memory=4GB file://$PWD/job_script.sh
+jobsub_submit -G nova -N 600 --resource-provides=usage_model=DEDICATED,OPPORTUNISTIC --role=Analysis --expected-lifetime=3h --memory=4GB file://$PWD/job_script.sh
