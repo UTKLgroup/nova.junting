51c66a4d3b7f29ab072787f7a7c424bb0008260e
juntingh <junting.huang@gmail.com>
2018-04-02 10:52:21 -0500

    added plot_particle_position()

--- testbeam/secondary_beam/secondary.py
+++ testbeam/secondary_beam/secondary.py
@@ -1,5 +1,10 @@
+from rootalias import *
 import math
 from math import cos, pi
+
+
+FIGURE_DIR = 'figures'
+
 
 def convert_rgb_color(rgbs):
     print(','.join(['{:.2f}'.format(rgb / 255.) for rgb in rgbs]))
@@ -35,8 +40,43 @@ def calculate_component_position():
     print(450. * (1. - 1. / cos(1.31 * pi / 180.)))
 
 
+def plot_particle_position():
+    tf = TFile('LAPPD_MC_1g.root')
+
+    detectors = ['CollUpstream', 'Scraper1', 'Scraper2', 'CollDownstream', 'NovaTarget']
+    h_xy = TH2D('h_xy', 'h_xy', 120, -3, 3, 120, -2, 4)
+    h_pxpy = TH2D('h_pxpy', 'h_pxpy', 40, -4, 4, 40, -4, 4)
+
+    for particle in tf.Get('VirtualDetector/{}'.format(detectors[4])):
+        # print(int(particle.PDGid))
+        # if int(particle.PDGid) == 2212 or 211:
+        # if int(particle.PDGid) == 211:
+        # if int(particle.PDGid) == 2212:
+        # if int(particle.PDGid) == -13:
+        h_xy.Fill(particle.x / 1000., particle.y / 1000.)
+        h_pxpy.Fill(particle.Px / 1000., particle.Py / 1000.)
+
+    c1 = TCanvas('c1', 'c1', 800, 600)
+    set_margin()
+    gPad.SetRightMargin(0.15)
+    gPad.SetLogz()
+
+    set_h2_color_style()
+    # set_h2_style(h_xy)
+    # h_xy.Draw('colz')
+
+    set_h2_style(h_pxpy)
+    h_pxpy.Draw('colz')
+
+    c1.Update()
+    c1.SaveAs('{}/plot_particle_position.pdf'.format(FIGURE_DIR))
+    input('Press any key to continue.')
+
+
 # 20180331_secondary_beam
-calculate_component_position()
+gStyle.SetOptStat(0)
+plot_particle_position()
+# calculate_component_position()
 # convert_rgb_color([201, 96, 35]) # quadrupole, color based on native values from mac color meter
 # convert_rgb_color([9, 78, 149])  # dipole
 # convert_rgb_color([78, 139, 50]) # trim
