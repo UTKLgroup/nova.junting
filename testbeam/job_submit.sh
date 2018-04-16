3178f936bccc235acafa9cf2c43032d0fd45292e
juntingh <junting.huang@gmail.com>
2018-04-16 17:54:30 -0500

    removed the memory requirement

--- testbeam/job_submit.sh
+++ testbeam/job_submit.sh
@@ -1,1 +1,1 @@
-jobsub_submit -G nova -N 900 --resource-provides=usage_model=DEDICATED,OPPORTUNISTIC --role=Analysis --expected-lifetime=3h --memory=4GB file://$PWD/job_script.sh
+jobsub_submit -G nova -N 900 --resource-provides=usage_model=DEDICATED,OPPORTUNISTIC --role=Analysis --expected-lifetime=3h file://$PWD/job_script.sh
