febefb8657cdfb9617bf58f0fe5ee973532cf1a4
juntingh <junting.huang@gmail.com>
2018-08-10 10:53:30 -0500

    played with first example of tensorflow

--- nnbar/cosmic.py
+++ nnbar/cosmic.py
@@ -12,3 +12,46 @@ fashion_mnist = keras.datasets.fashion_mnist
 
 print('train_images.shape = {}'.format(train_images.shape))
 print('len(train_labels) = {}'.format(len(train_labels)))
+print('train_labels = {}'.format(train_labels))
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+train_images = train_images / 255.
+test_images = test_images / 255.
+
+# plt.figure()
+# plt.imshow(train_images[0])
+# plt.colorbar()
+# plt.gca().grid(False)
+
+# plt.figure(figsize=(10,10))
+# for i in range(25):
+#     plt.subplot(5,5,i+1)
+#     plt.xticks([])
+#     plt.yticks([])
+#     plt.grid('off')
+#     plt.imshow(train_images[i], cmap=plt.cm.binary)
+#     plt.xlabel(class_names[train_labels[i]])
+
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28, 28)),
+    keras.layers.Dense(128, activation=tf.nn.relu),
+    keras.layers.Dense(10, activation=tf.nn.softmax)
+])
+
+model.compile(optimizer=tf.train.AdamOptimizer(),
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(train_images, train_labels, epochs=5)
+test_loss, test_acc = model.evaluate(test_images, test_labels)
+
+print('Test accuracy:', test_acc)
+
+predictions = model.predict(test_images)
+print('predictions[0] = {}'.format(predictions[0]))
+print('np.argmax(predictions[0]) = {}'.format(np.argmax(predictions[0])))
+print('test_labels[0] = {}'.format(test_labels[0]))
+
+# plt.show()
