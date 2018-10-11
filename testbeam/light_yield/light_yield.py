07d8964c57bc3c3433bcbd7c9a05475cf7508d12
juntingh <junting.huang@gmail.com>
2018-10-11 16:04:44 -0500

    change figure dir to beamer figures

--- testbeam/light_yield/light_yield.py
+++ testbeam/light_yield/light_yield.py
@@ -3,7 +3,7 @@ import scipy.constants
 import csv
 
 
-FIGURE_DIR = './figures'
+FIGURE_DIR = '/Users/juntinghuang/beamer/20181005_testbeam_light_yield_setup/figures'
 DATA_DIR = './data'
 
 
@@ -195,8 +195,8 @@ def plot_npe_vs_led_voltage():
 gStyle.SetOptStat(0)
 # plot_gain('F1ch200008.txt')
 # plot_gain('F1ch200015.txt')
-plot_gain('F1ch200011.txt')
+# plot_gain('F1ch200011.txt')
 # plot_gain_vs_hv()
 # plot_mean_per_pe_vs_hv()
 # plot_sigma2_vs_mu()
-# plot_npe_vs_led_voltage()
+plot_npe_vs_led_voltage()
