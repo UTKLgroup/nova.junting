b0091227c938bc0d5319c83cc16946f4ccf1298f
juntingh <junting.huang@gmail.com>
2018-11-02 09:38:12 -0500

    increase the job time limit from 6h to 8h

--- testbeam/job_submit.sh
+++ testbeam/job_submit.sh
@@ -1,1 +1,1 @@
-jobsub_submit -G nova -N 2000 --resource-provides=usage_model=DEDICATED,OPPORTUNISTIC --role=Analysis --expected-lifetime=6h file://$PWD/job_script.sh
+jobsub_submit -G nova -N 2000 --resource-provides=usage_model=DEDICATED,OPPORTUNISTIC --role=Analysis --expected-lifetime=8h file://$PWD/job_script.sh
