795c20435e920574ab76c4bd70e2263f05f42f5b
juntingh <junting.huang@gmail.com>
2018-11-22 23:43:03 -0600

    use merge_tree.trigger.py and save_particle_to_root.py, do not use start_line

--- testbeam/job_script.sh
+++ testbeam/job_script.sh
@@ -18,20 +18,25 @@ export G4LEVELGAMMADATA=/cvmfs/nova.opensciencegrid.org/externals/g4photon/v4_3_
 export G4RADIOACTIVEDATA=/cvmfs/nova.opensciencegrid.org/externals/g4radiative/v5_1_1/RadioactiveDecay5.1.1
 export G4REALSURFACEDATA=/cvmfs/nova.opensciencegrid.org/externals/g4surface/v1_0/NULL/RealSurface1.0
 
-PROCESS_START=0
+# PROCESS_START=0
+PROCESS_START=30000
 # B_FIELD=-0.45
 B_FIELD=-0.9
 # B_FIELD=-1.35
 # B_FIELD=-1.8
+# B_FIELD=0.9
 
-ifdh cp /pnfs/nova/persistent/users/junting/testbeam/merge_tree.py ./merge_tree.py
-ifdh cp /pnfs/nova/persistent/users/junting/testbeam/save_particle_to_csv.py ./save_particle_to_csv.py
+# ifdh cp /pnfs/nova/persistent/users/junting/testbeam/merge_tree.py ./merge_tree.py
+# ifdh cp /pnfs/nova/persistent/users/junting/testbeam/save_particle_to_csv.py ./save_particle_to_csv.py
+ifdh cp /pnfs/nova/persistent/users/junting/testbeam/merge_tree.trigger.py ./merge_tree.trigger.py
+ifdh cp /pnfs/nova/persistent/users/junting/testbeam/save_particle_to_root.py ./save_particle_to_root.py
 ifdh cp /pnfs/nova/persistent/users/junting/testbeam/beamline.py.in ./beamline.py.in
 
-PARTICLE=pi+
+PARTICLE=proton
+# PARTICLE=pi+
 MOMENTUM=64000
-EVENT_COUNT_PER_JOB=10000
-JOB_COUNT_PER_SPILL=100
+EVENT_COUNT_PER_JOB=20000
+JOB_COUNT_PER_SPILL=50
 FIRST=$((((${PROCESS_START} + ${PROCESS}))* ${EVENT_COUNT_PER_JOB}))
 LAST=$((${FIRST} + $EVENT_COUNT_PER_JOB - 1))
 echo "PROCESS is: $PROCESS"
@@ -42,13 +47,20 @@ JOB_COUNT=$((${PROCESS_START} + ${PROCESS} + 1))
 EVENT_COUNT_PER_SPILL=$((${EVENT_COUNT_PER_JOB} * ${JOB_COUNT_PER_SPILL}))
 
 g4bl beamline.py.in first=${FIRST} last=${LAST} particle=${PARTICLE} momentum=${MOMENTUM} b_field=${B_FIELD}
-python merge_tree.py beam.root --subspillnumber $JOB_COUNT --subspillcount $JOB_COUNT_PER_SPILL --spillsize $EVENT_COUNT_PER_SPILL
-python save_particle_to_csv.py MergedAtstart_linebeam.root
+python merge_tree.trigger.py beam.root -T tof_ds --subspillnumber $JOB_COUNT --subspillcount $JOB_COUNT_PER_SPILL --spillsize $EVENT_COUNT_PER_SPILL
 
 # chmod 766 MergedAtstart_linebeam.root
 # chmod 766 MergedAtstart_linebeam.pickle
 # ifdh cp MergedAtstart_linebeam.root /pnfs/nova/scratch/users/junting/g4bl.${B_FIELD}.${PARTICLE}.${MOMENTUM}.MergedAtstart_linebeam.${JOB_COUNT}.root
 # ifdh cp MergedAtstart_linebeam.pickle /pnfs/nova/scratch/users/junting/g4bl.${B_FIELD}.${PARTICLE}.${MOMENTUM}.MergedAtstart_linebeam.${JOB_COUNT}.pickle
 
-chmod 766 MergedAtstart_linebeam.root.csv
-ifdh cp MergedAtstart_linebeam.root.csv /pnfs/nova/scratch/users/junting/g4bl.b_${B_FIELD}T.${PARTICLE}.${MOMENTUM}.MergedAtstart_linebeam.${JOB_COUNT}.root.csv
+# python save_particle_to_csv.py MergedAtstart_linebeam.root
+# chmod 766 MergedAtstart_linebeam.root.csv
+# ifdh cp MergedAtstart_linebeam.root.csv /pnfs/nova/scratch/users/junting/g4bl.b_${B_FIELD}T.${PARTICLE}.${MOMENTUM}.MergedAtstart_linebeam.${JOB_COUNT}.root.csv
+
+# chmod 766 MergedAttof_dsbeam.root
+# ifdh cp MergedAttof_dsbeam.root /pnfs/nova/scratch/users/junting/g4bl.b_${B_FIELD}T.${PARTICLE}.${MOMENTUM}.MergedAttof_dsbeam.root.${JOB_COUNT}.root
+
+python save_particle_to_root.py MergedAttof_dsbeam.root
+chmod 766 MergedAttof_dsbeam.trigger.root
+ifdh cp MergedAttof_dsbeam.trigger.root /pnfs/nova/scratch/users/junting/g4bl.b_${B_FIELD}T.${PARTICLE}.${MOMENTUM}.MergedAttof_dsbeam.trigger.root.${JOB_COUNT}.root
