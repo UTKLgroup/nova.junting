ed3cb2266403ba1d78a23a02b4d2c4b04095be32
juntingh <junting.huang@gmail.com>
2019-02-22 09:42:03 -0600

    added option of using different computer for evd.py

--- testbeam/evd.py
+++ testbeam/evd.py
@@ -7,20 +7,22 @@ import cv2
 import numpy as np
 
 
-def position():
+def position(computer_name):
     print(pyautogui.position())
     pyautogui.moveTo(80, 260)
     pyautogui.click()
     pyautogui.moveTo(750, 570)
     pyautogui.click()
     pyautogui.dragTo(1501, 950, button='left')
+    if computer_name == 'macbook':
+        pyautogui.dragTo(1000, 596, button='left')
     # pyautogui.hotkey('command', 'shift', '5')
     # pyautogui.moveTo(180, 420)
     # pyautogui.mouseDown()
     # pyautogui.drag(1070, 240, 1, button='left')
 
 
-def take_screen_shot(figure_dir, prefix, timestamp):
+def take_screen_shot(figure_dir, prefix, timestamp, computer_name):
     filename = 'evd.'
     if prefix:
         filename += prefix + '.'
@@ -31,6 +33,11 @@ def take_screen_shot(figure_dir, prefix, timestamp):
     y0 = 420
     width = 1130
     height = 250
+    if computer_name == 'macbook':
+        x0 = 200
+        y0 = 290
+        width = 650
+        height = 150
     call('screencapture -x -t pdf -R{},{},{},{} {}/{}'.format(x0, y0, width, height, figure_dir, filename), shell=True)
 
 
@@ -39,7 +46,8 @@ if __name__ == '__main__':
     parser.add_argument('-d', '--figure_dir', help='figure dir', default='.')
     parser.add_argument('-p', '--prefix', help='figure name prefix', default='')
     parser.add_argument('-t', '--timestamp', help='add timestamp in filename', action='store_true')
+    parser.add_argument('-c', '--computer_name', help='on which computer', default='imac')
     args = parser.parse_args()
 
-    position()
-    take_screen_shot(args.figure_dir, args.prefix, args.timestamp)
+    position(args.computer_name)
+    take_screen_shot(args.figure_dir, args.prefix, args.timestamp, args.computer_name)
